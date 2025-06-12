68c390c4094cf63fede7166a3f17650dfd13e4fc
4nttiJokela <antti.jokela2@gmail.com>
2025-06-12 15:29:17 +0300

    Viikko 1

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -8,7 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Noni tehdääs ohjelmia" );
-        
+        Animal myAnimal = new Animal("Pikseli", "hedgehog", 13);
+
+        System.out.println(myAnimal.name);
+        System.out.println(myAnimal.species);
+        System.out.println(myAnimal.age);
     }
 }
