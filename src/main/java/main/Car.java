f2df279fc6907a426676c502ce320cf22d6b0c33
4nttiJokela <antti.jokela2@gmail.com>
2025-06-25 16:36:58 +0300

    Viikko 2.1

--- src/main/java/main/Car.java
+++ src/main/java/main/Car.java
@@ -12,7 +12,7 @@ public class Car {
     }
 
     public void status() {
-        System.out.println("Auto: "+ brand + " " + model + ", nopeus " + speed + " km/h");
+        System.out.println("Auto: "+ brand + " " + model + ", Nopeus " + speed + " km/h");
     }
 
     public void setBrand(String brand) {
